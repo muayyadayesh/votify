4309b81a7452d4eb9295b862ecb599a206d93f8d
muayyadayesh <muayyadayesh@gmail.com>
2021-11-18 12:46:29 +0200

    Models implementation

--- votingapp/models.py
+++ votingapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+class Poll(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=256, unique=True)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    last_update = models.DateTimeField(default=timezone.now())
+
+    def set_last_update(self):
+        self.last_update = timezone.now()
+        self.save()
+
+    #reverse after actions
+    def get_absolute_url(self):
+        return reverse("app:poll_details", kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+#Answer object which has Answer-Poll (many-1) relationship
+class Answer(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    isRequired = models.BooleanField()
+    votes = models.IntegerField(default=0)
+
+    def Increase_vote(self):
+        self.votes += 1
+        self.save()
+
+    def __str__(self):
+        return self.title
