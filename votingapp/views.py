d7550703609f9ec92e0da38c92990ecfb9381efc
muayyadayesh <muayyadayesh@gmail.com>
2021-11-18 12:23:51 +0200

    Configruing the rest of View Classes & Defs

--- votingapp/views.py
+++ votingapp/views.py
@@ -1,26 +1,58 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.utils import timezone
+from .models import *
+from .forms import *
 
 # Create your views here.
 
+#Homepage
 class home(TemplateView):
     template_name = 'home.html'
 
+#List of all polls
 class poll_list (ListView):
     model = Poll
     def get_queryset(self):
         return Poll.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
 
-class poll_create(LoginRequiredMixin, CreateView):
+#View poll details by the author: LoginRequired
+class poll_details(LoginRequiredMixin, DetailView):
+    model = Poll
+    template_name = 'poll_details.html'
+
+#Create new poll: LoginRequired
+class poll_new(LoginRequiredMixin, CreateView):
     form_class = PollForm
     model = Poll
 
     login_url = '/login'
     redirect_field_name = 'votingapp/poll_details.html'
 
+#Update the poll: LoginRequired
 class poll_update(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     redirect_field_name = 'votingapp/poll_details.html'
 
     form_class = PollForm
     model = Poll
+
+#Delete poll using deleteview: LoginRequired
+class poll_delete(LoginRequiredMixin, DeleteView):
+    model = Poll
+    success_url = reverse_lazy('app:poll_list')
+
+#Poll result, appears after voting action
+class poll_result(DetailView):
+    model = Poll
+    template_name = 'poll_result.html'
+
+#No auth required here
+def poll_vote(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+    form = VoteForm(data = request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('votingapp:poll_result', pk=poll.pk)
