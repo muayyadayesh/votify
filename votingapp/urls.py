f616d7969212a4ff5994309d667634cd99817064
muayyadayesh <muayyadayesh@gmail.com>
2021-11-18 11:52:41 +0200

    Urls & CBVs modifications

--- votingapp/urls.py
+++ votingapp/urls.py
@@ -7,9 +7,11 @@ app_name = 'votingapp'
 #Polls urlPatterns
 urlpatterns = [
     path('', home.as_view(), name='home'),
-    path('polls/', poll_list.as_view(), name='poll_list'),
-    path('polls/new/', create_poll.as_view(), name='create_poll'),
-    path('polls/<int:pk>/', pull_details.as_view(), name='pull_details'),
-    path('polls/<int:pk>/vote', poll_vote.as_view(), name='poll_vote'),
-    path('polls/<int:pk>/result', poll_result.as_view(), name='poll_result'),
+    path('poll/', poll_list.as_view(), name='poll_list'),
+    path('poll/<int:pk>/', poll_details.as_view(), name='poll_details'),
+    path('poll/new/', poll_create.as_view(), name='poll_create'),
+    path('poll/<int:pk>/delete', poll_delete.as_view(), name='poll_delete'),
+    path('poll/<int:pk>/update_poll', poll_update.as_view(), name='poll_update'),
+    path('poll/<int:pk>/vote', poll_vote.as_view(), name='poll_vote'),
+    path('poll/<int:pk>/result', poll_vote.as_view(), name='poll_result'),
 ]
--- votingapp/views.py
+++ votingapp/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 
 # Create your views here.
+
+class home(TemplateView):
+    template_name = 'home.html'
+
+class poll_list (ListView):
+    model = Poll
+    def get_queryset(self):
+        return Poll.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
+
+class poll_create(LoginRequiredMixin, CreateView):
+    form_class = PollForm
+    model = Poll
+
+    login_url = '/login'
+    redirect_field_name = 'votingapp/poll_details.html'
+
+class poll_update(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'votingapp/poll_details.html'
+
+    form_class = PollForm
+    model = Poll
